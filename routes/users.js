1ed02989a459f4af4a65656eba6aa542b7cf2954
southwestmogrown <mail@southwestmogrown.com>
2021-06-01 16:48:37 -0500

    finished user registration form

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const {check} = require('express-validator');
 const {asyncHandler, handleValidationErrors, csrfProtection} = require('./utils');
 const { User } = require('../db/models')
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Registration Page');
+router.get('/', function(req, res, next) { // User homepage
+  res.send('User HomePage');
 });
 
-router.post('/users/register')
+router.get('/register', csrfProtection, asyncHandler(async(req, res) => { // grabbing the registration page
+  res.render('user-register', { title: "Registration", csrfToken: req.csrfToken()})
+}));
+
+router.post('/register', csrfProtection, asyncHandler(async(req, res) => { // creates the new user and redirect them to their homepage
+
+}));
+
+router.get('/login')
 
 module.exports = router;
