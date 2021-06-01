f81bc5d91404796581e2b78cbad0af210164b21b
southwestmogrown <mail@southwestmogrown.com>
2021-06-01 14:32:05 -0500

    added users table

---
+++ db/migrations/20210601181349-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userName: {
+        allowNull: false,
+        type: Sequelize.STRING(75)
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+        unique: true
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      professionalUser: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
