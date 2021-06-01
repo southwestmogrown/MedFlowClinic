23b8decc21f6040234f2f5702c9242e1d6395efa
Vivian C <baongocche@gmail.com>
2021-06-01 14:11:24 -0700

    created utils and user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const {check} = require('express-validator');
+const {asyncHandler, handleValidationErrors, csrfProtection} = require('./utils');
+const { User } = require('../db/models')
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Registration Page');
 });
+
+router.post('/users/register')
 
 module.exports = router;
