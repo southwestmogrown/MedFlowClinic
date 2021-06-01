defb8c3845de6db5b6932d0838320204e67200d5
Richard Huang <rzhuangsg115@gmail.com>
2021-06-01 14:59:27 -0500

    create seeder file

---
+++ db/seeders/20210601195231-demoUser.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+      return queryInterface.bulkInsert('Users', [
+        {userName:"Demo-user", email:"demo@user.com", professionalUser: true}
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+      return queryInterface.bulkDelete('Users', null, {});
+  }
+};
