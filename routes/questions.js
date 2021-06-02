e9b399d91fbfb76eae61e84d13fc3a3e906afbfb
Richard Huang <rzhuangsg115@gmail.com>
2021-06-02 16:51:41 -0500

    rendering page with associated answers

--- routes/questions.js
+++ routes/questions.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const {check, validationResult} = require('express-validator');
 const {asyncHandler, csrfProtection} = require('./utils');
 const {requireAuth} = require("../auth");
-const {Question} = require("../db/models");
+const {Question, Answer} = require("../db/models");
+
+//get question by id and render a page with the question and its answers
+router.get("/:id(\\d+)", asyncHandler(async (req, res) => {
+    const question = await Question.findByPk(req.params.id, {
+        include: Answer
+    });
+
+    res.render("question-page", {question});
+}));
 
 module.exports = router;
