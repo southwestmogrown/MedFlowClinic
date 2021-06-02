55b5e80ad2b18122fda23e896c01a1144ee7baee
Richard Huang <rzhuangsg115@gmail.com>
2021-06-02 18:46:37 -0500

    made asking question feature

--- routes/questions.js
+++ routes/questions.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {check, validationResult} = require('express-validator');
 const {asyncHandler, csrfProtection} = require('./utils');
 const {requireAuth} = require("../auth");
-const {Question, Answer} = require("../db/models");
+const {Question, Answer, User} = require("../db/models");
 
 //get question by id and render a page with the question and its answers
 router.get("/:id(\\d+)", asyncHandler(async (req, res) => {
@@ -14,14 +14,39 @@ router.get("/:id(\\d+)", asyncHandler(async (req, res) => {
     res.render("question-page", {question});
 }));
 
+const questionValidator = [
+    check("question")
+        .exists({checkFalsy: true})
+        .withMessage("Please enter a question")
+]
+
 //render a page where user can input their question
-router.get("/add", csrfProtection, asyncHandler(async (req, res) => {
+router.get("/add", csrfProtection, requireAuth, asyncHandler(async (req, res) => {
     res.render("question-form", {csrfToken: req.csrfToken()});
 }));
 
 //posts question when submitted
-router.post("/add", csrfProtection, asyncHandler(async (req, res) => {
+router.post("/add", csrfProtection, requireAuth, questionValidator, asyncHandler(async (req, res) => {
     const {question} = req.body;
+    const {userId} = req.session.auth;
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+        const newQuestion = await Question.create({
+            question,
+            voteCount: 0,
+            userId
+        });
+
+        res.redirect(`/questions/${newQuestion.id}`);
+    } else {
+        const errors = validatorErrors.array().map((error) => error.msg);
+        res.render('question-form', {
+            errors,
+            csrfToken: req.csrfToken()
+        });
+    }
 }));
 
 module.exports = router;
