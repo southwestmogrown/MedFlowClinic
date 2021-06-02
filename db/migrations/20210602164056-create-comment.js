e30e36e8fc266fd893666930bec927eb625f3e53
Richard Huang <rzhuangsg115@gmail.com>
2021-06-02 11:42:06 -0500

    create migration for comments table

---
+++ db/migrations/20210602164056-create-comment.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      answerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: "Answers"}
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: "Users"}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Comments');
+  }
+};
