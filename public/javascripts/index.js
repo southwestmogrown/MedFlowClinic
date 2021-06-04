706d8471493baef7443e369c933ce93dc5a05d39
Richard Huang <rzhuangsg115@gmail.com>
2021-06-04 17:06:10 -0500

    refactor upvote downvote eventlistener to not rely on nodelist index

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,49 +1,72 @@
 window.addEventListener("DOMContentLoaded", (event)=>{
     const upVote = document.querySelectorAll(".upvote-button");
     const downVote = document.querySelectorAll(".downvote-button");
-    const counter = document.querySelectorAll(".votes-number");
+    // counters = document.querySelectorAll(".votes-number");
 
-    //addEventListener to each upvote and downvote button
+    const upVoteAns = document.querySelectorAll(".answer-upvote-button");
+    const downVoteAns = document.querySelectorAll(".answer-downvote-button");
+    const counterAns = document.querySelectorAll(".answer-vote-count");
+    console.log(upVote)
+    //console.log(counters)
+    //addEventListener to each upvote and downvote button for questions
 
-    for (let i = 0; i < upVote.length; i++) {   //Put eventlistener on each upvote button available
+    for (let i = 0; i < upVote.length; i++) {   //Put eventlistener on each buttons
         upVote[i].addEventListener("click", async (e) => {
             //extract id number from upvote button
             const upVoteArr = upVote[i].id.split("");
             const idNum = parseInt(upVoteArr[upVoteArr.length - 1]);
+            const counter = document.getElementById(`counter-${idNum}`)
 
             const result = await fetch(`/voting/upvote/${idNum}`, {
                 method: "PATCH"
             });
             const json = await result.json();
 
-            counter[i].innerHTML = json.voteCount;
+            counter.innerHTML = json.voteCount;
         });
-    }
 
-    for (let j = 0; j < downVote.length; j++) {
-        downVote[j].addEventListener("click", async (e) => {
+        downVote[i].addEventListener("click", async (e) => {
             //extract id number from downvote button
-            const downVoteArr = downVote[j].id.split("");
+            const downVoteArr = downVote[i].id.split("");
             const idNum = parseInt(downVoteArr[downVoteArr.length - 1]);
+            const counter = document.getElementById(`counter-${idNum}`)
 
             const result = await fetch(`/voting/downvote/${idNum}`, {
                 method: "PATCH"
             });
             const json = await result.json();
 
-            counter[j].innerHTML = json.voteCount;
+            counter.innerHTML = json.voteCount;
         });
     }
 
-    /* downVote.addEventListener("click", async (e) => {
-        const upVoteArr = upVote.id.split("");
-        const idNum = parseInt(upVoteArr[upVoteArr.length - 1]);
+    //addeventlistener to upvote downvote buttons in answers
+    //Need to be able to grab the id number at the end of the ids in each answer button
+    for (let iAns = 0; iAns < upVoteAns.length; iAns++) {
+        upVoteAns[iAns].addEventListener("click", async (e) => {
+            const upVoteArr = upVoteAns[iAns].id.split("");
+            const idNum = parseInt(upVoteArr[upVoteArr.length - 1]);
 
-        const result = await fetch(`/voting/downvote/${idNum}`, {
-            method: "PATCH"
+            const result = await fetch(`/voting/upvote/answer/${idNum}`, {
+                method: "PATCH"
+            });
+            const json = await result.json();
+
+            counterAns[iAns].innerHTML = json.voteCount;
         });
-        const json = await result.json();
 
-        counter.innerHTML = json.voteCount;
-    }); */
-})
+        downVoteAns[iAns].addEventListener("click", async (e) => {
+            const downVoteArr = downVoteAns[iAns].id.split("");
+            const idNum = parseInt(downVoteArr[downVoteArr.length - 1]);
+
+            const result = await fetch(`/voting/downvote/answer/${idNum}`, {
+                method: "PATCH"
+            });
+
+            const json = await result.json();
+
+            counterAns[iAns].innerHTML = json.voteCount;
+        });
+    }
+
+});
--- routes/voting.js
+++ routes/voting.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {asyncHandler} = require('./utils');
-const {Question, Answer, User} = require("../db/models");
+const {Question, Answer} = require("../db/models");
 const {requireAuth} = require("../auth");
 
 router.patch("/upvote/:id(\\d+)", requireAuth, asyncHandler(async (req, res) => {
@@ -20,6 +20,24 @@ router.patch("/downvote/:id(\\d+)", requireAuth, asyncHandler(async (req, res) =
     question.save({fields: ["voteCount"]});
 
     res.json(question);
+}));
+
+router.patch("/upvote/answer/:id(\\d+)", requireAuth, asyncHandler(async (req, res) => {
+    const answer = await Answer.findByPk(req.params.id);
+
+    answer.voteCount += 1;
+    answer.save({fields: ["voteCount"]});
+
+    res.json(answer);
+}));
+
+router.patch("/downvote/answer/:id(\\d+)", requireAuth, asyncHandler(async (req, res) => {
+    const answer = await Answer.findByPk(req.params.id);
+
+    answer.voteCount -= 1;
+    answer.save({fields: ["voteCount"]});
+
+    res.json(answer);
 }));
 
 module.exports = router;
