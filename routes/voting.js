dc1728f191b71946bcfe4fcbbf193817e9ebc092
Richard Huang <rzhuangsg115@gmail.com>
2021-06-04 14:07:48 -0500

    updating voteCount

--- routes/voting.js
+++ routes/voting.js
@@ -7,7 +7,10 @@ const {requireAuth} = require("../auth");
 const vote = {score: 99};
 
 router.patch("/upvote/:id(\\d+)", requireAuth, asyncHandler(async (req, res) => {
-    console.log(`HHHHHH`)
+    //upvote button should upvote question 7 data and only do it once
+    const question = await Question.findByPk(req.params.id);
+    question.voteCount++;
+
     res.json(vote);
 }));
 
