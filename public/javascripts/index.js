fe6f8066f4d3795505e3fba1108cb1cc42354ad1
Richard Huang <rzhuangsg115@gmail.com>
2021-06-04 15:45:08 -0500

    can upvote and downvote questions

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,9 +5,9 @@ window.addEventListener("DOMContentLoaded", (event)=>{
 
     //addEventListener to each upvote and downvote button
 
-    for (let i = 0; i < upVote.length; i++) {   //Put eventlistener on each button available
+    for (let i = 0; i < upVote.length; i++) {   //Put eventlistener on each upvote button available
         upVote[i].addEventListener("click", async (e) => {
-            //extract id number from button
+            //extract id number from upvote button
             const upVoteArr = upVote[i].id.split("");
             const idNum = parseInt(upVoteArr[upVoteArr.length - 1]);
 
@@ -17,6 +17,21 @@ window.addEventListener("DOMContentLoaded", (event)=>{
             const json = await result.json();
 
             counter[i].innerHTML = json.voteCount;
+        });
+    }
+
+    for (let j = 0; j < downVote.length; j++) {
+        downVote[j].addEventListener("click", async (e) => {
+            //extract id number from downvote button
+            const downVoteArr = downVote[j].id.split("");
+            const idNum = parseInt(downVoteArr[downVoteArr.length - 1]);
+
+            const result = await fetch(`/voting/downvote/${idNum}`, {
+                method: "PATCH"
+            });
+            const json = await result.json();
+
+            counter[j].innerHTML = json.voteCount;
         });
     }
 
