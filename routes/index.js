baa30ac7f218d6fff93e6b94096649f8185e4e86
southwestmogrown <mail@southwestmogrown.com>
2021-06-04 13:13:32 -0500

    working on adding userName to questions

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {asyncHandler} = require('./utils');
-const {Question} = require("../db/models");
+const {Question, User} = require("../db/models");
 
 /* GET home page. */
 //Render 5 most recently asked question
 router.get('/', asyncHandler(async function(req, res, next) {
   const questions = await Question.findAll({
+    include: User,
     order: [["createdAt", "DESC"]],
     limit: 10
   });
